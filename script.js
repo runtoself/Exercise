659cc1f3c78fbe77eee0b420f37bf2b882b04a47
runtoself <672787887@qq.com>
2024-03-14 21:09:16 +0800

    exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHallo(){
+  var response = prompt("What's your name?");
+  alert('Hallo,' + response + ',have a nice day!');
+}
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = '25px';
+  x.style.color = 'red';
+}
